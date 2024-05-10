4215e2b4f7ed4d25413aa6da1e29cfa6643944d2
Daniele-Perottoni <91191654+Daniele-Perottoni@users.noreply.github.com>
2024-05-10 13:18:39 +0000

    base project

---
+++ provadb/src/main/java/it/marconi/provadb/ProvadbApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.provadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvadbApplication.class, args);
+	}
+
+}
---
+++ provadb/src/test/java/it/marconi/provadb/ProvadbApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconi.provadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
